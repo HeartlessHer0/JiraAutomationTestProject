3bda1d1945cd930fecb3b230c737523a7571da5c
Sergey Zhigar <jedai38@gmail.com>
2022-07-15 17:28:12 +0300

    Logger before POM updating

--- src/test/java/steps/defs/AttachmentUIDefs.java
+++ src/test/java/steps/defs/AttachmentUIDefs.java
@@ -5,6 +5,8 @@ import configuration.ReadProperties;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.testng.Assert;
@@ -16,6 +18,7 @@ import java.time.Duration;
 public class AttachmentUIDefs extends BaseCucumberTest {
    private WaitsService waitsService = new WaitsService(driver, Duration.ofSeconds(ReadProperties.timeout()));
    private String testComment = "Test Comment";
+    private static final Logger logger = LogManager.getLogger(AttachmentUIDefs.class);
 
     @When("User add Test Image to Issue")
     public void userAddTestImageToIssue() throws InterruptedException {
@@ -23,6 +26,7 @@ public class AttachmentUIDefs extends BaseCucumberTest {
 
         boardPage.getOpenIssueButtonLocator("ImageAndCommentTest").click();
         boardPage.getFileUploadingLocator().sendKeys(path);
+        logger.info("Test image added to Test Issue");
         Thread.sleep(3000);
     }
 
@@ -31,17 +35,21 @@ public class AttachmentUIDefs extends BaseCucumberTest {
         boardPage.getCreateIssueButton("К выполнению").click();
         boardPage.getIssueNameFieldLocator().sendKeys("ImageAndCommentTest");
         boardPage.getIssueNameFieldLocator().sendKeys(Keys.ENTER);
+        logger.info("Test issue created");
         boardPage.getCurrentBoardButton().click();
+        logger.info("Page reloaded");
     }
 
     @Then("User see added Image in Issue")
     public void userSeeAddedImageInIssue() {
         Assert.assertTrue(boardPage.getImageLocator().isDisplayed());
+        logger.info("Test image is on the page");
     }
 
     @When("User open test Issue")
     public void userOpenTestIssue() {
         boardPage.getOpenIssueButtonLocator("ImageAndCommentTest").click();
+        logger.info("Test Issue is open");
     }
 
     @And("User delete Test Image from Issue")
@@ -49,11 +57,13 @@ public class AttachmentUIDefs extends BaseCucumberTest {
         boardPage.getImageActionsLocator().click();
         boardPage.getImageDeleteButton().click();
         boardPage.getConfirmDeleteImageButton().click();
+        logger.info("Test Image deleted from the Test Issue");
     }
 
     @Then("User see Issue without Test Image")
     public void userSeeIssueWithoutTestImage() {
        Assert.assertTrue(waitsService.waitForInvisibilityElement(boardPage.getImageInvisLocator()));
+        logger.info("Test Image is not on the Issue");
     }
 
     @And("User add comment")
@@ -61,21 +71,25 @@ public class AttachmentUIDefs extends BaseCucumberTest {
         boardPage.getIssueCommentFieldLocator().click();
         boardPage.getActivatedCommentFieldLocator().sendKeys(testComment);
         boardPage.getCommentOrDescriptionSaveButtonLocator().click();
+        logger.info("Comment added to the Issue");
     }
 
     @Then("User see added comment in Issue")
     public void userSeeAddedCommentInIssue() {
         Assert.assertTrue(boardPage.getCommentLocator(testComment).isDisplayed());
+        logger.info("Comment is on the page");
     }
 
     @And("User delete Test Comment")
     public void userDeleteTestComment() {
         boardPage.getImageDeleteButton().click();
         boardPage.getConfirmDeleteImageButton().click();
+        logger.info("Comment deleted from Issue");
     }
 
     @Then("User see Issue without Test Comment")
     public void userSeeIssueWithoutTestComment() {
         Assert.assertTrue(waitsService.waitForInvisibilityElement(By.xpath(boardPage.getCommentInvLocator(testComment))));
+        logger.info("Comment is not on the Issue");
     }
 }
--- src/test/java/steps/defs/LoginUIDefs.java
+++ src/test/java/steps/defs/LoginUIDefs.java
@@ -4,73 +4,92 @@ import baseEntities.BaseCucumberTest;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 
 public class LoginUIDefs extends BaseCucumberTest {
-    BaseCucumberTest baseCucumberTest;
-    
+    public  BaseCucumberTest baseCucumberTest;
+    private static final Logger logger = LogManager.getLogger(LoginUIDefs.class);
+
+
     @Given("Atlassian login page is open")
     public void atlassianLoginPageIsOpen() {
         baseCucumberTest.driver.get("https://id.atlassian.com/login");
+        logger.info("Atlassian Login Page is opened");
     }
 
     @When("User enter correct email and password")
     public void userEnterCorrectEmailAndPassword() {
         loginPage.getEmailInput().sendKeys("jedai38@gmail.com");
         loginPage.getSubmitButton().click();
+        logger.info("Correct email is entered");
         loginPage.getPswInput().sendKeys("MyPass4RegTrello2022");
         loginPage.getSubmitButton().click();
+        logger.info("Correct psw is entered");
+
     }
 
     @Then("User come to the Atlassian Start Page")
     public void userComeToTheAtlassianStartPage() {
         Assert.assertTrue(startPage.isPageOpened());
+        logger.info("Start Page is opened");
     }
 
     @When("User enter correct email and incorrect password")
     public void userEnterCorrectEmailAndIncorrectPassword() {
         loginPage.getEmailInput().sendKeys("jedai38@gmail.com");
         loginPage.getSubmitButton().click();
+        logger.info("Correct email is entered");
         loginPage.getPswInput().sendKeys("12345567");
         loginPage.getSubmitButton().click();
+        logger.info("Incorrect psw is entered");
     }
 
     @Then("User see error text area")
     public void userSeeErrorTextArea() {
         Assert.assertTrue(loginPage.getIncorrectPasswordOrEmailElement().isDisplayed());
+        logger.info("Login error message is displayed");
     }
 
     @When("User enter correct email and password is null")
     public void userEnterCorrectEmailAndPasswordIsNull() {
         loginPage.getEmailInput().sendKeys("jedai38@gmail.com");
         loginPage.getSubmitButton().click();
+        logger.info("Correct email is entered");
         loginPage.getPswInput().click();
         loginPage.getSubmitButton().click();
+        logger.info("Psw is Null");
     }
 
     @Then("User see password error message")
     public void userSeePasswordErrorMessage() {
         Assert.assertTrue(loginPage.getPasswordIsNullElement().isDisplayed());
+        logger.info("Password error message is displayed");
     }
 
     @When("User enter unregistered email")
     public void userEnterUnregisteredEmail() {
         loginPage.getEmailInput().sendKeys("some@mail");
         loginPage.getSubmitButton().click();
+        logger.info("Unregistered email is entered");
     }
 
     @Then("User see Registration button")
     public void userSeeRegistrationButton() {
         Assert.assertTrue(loginPage.getSignupSubmitButton().isDisplayed());
+        logger.info("Registration button is displayed");
     }
 
     @When("User dont enter email")
     public void userDontEnterEmail() {
         loginPage.getSubmitButton().click();
+        logger.info("Email is Null");
     }
 
     @Then("User see Email error message")
     public void userSeeEmailErrorMessage() {
         Assert.assertTrue(loginPage.getEmailIsNull().isDisplayed());
+        logger.info("Email error message is displayed");
     }
 }
