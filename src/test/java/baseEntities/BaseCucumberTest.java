fa71622489bfb2afd41d396d4100bdead0a5a520
Sergey Zhigar <jedai38@gmail.com>
2022-07-16 18:44:14 +0300

    FIX! Go team! Not Final secure fields

--- src/test/java/baseEntities/BaseCucumberTest.java
+++ src/test/java/baseEntities/BaseCucumberTest.java
@@ -13,9 +13,9 @@ import java.util.ArrayList;
 
 
 public class BaseCucumberTest {
-    public static final String LOGIN = System.getProperty("login");
-    public static final String PASSWORD = System.getProperty("password");
-    public static final String TOKEN = System.getProperty("token");
+    public static String LOGIN = System.getProperty("login");
+    public static String PASSWORD = System.getProperty("password");
+    public static String TOKEN = System.getProperty("token");
 
     public static WebDriver driver;
 
