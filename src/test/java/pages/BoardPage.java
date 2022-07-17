da8c4eebd54eed1f76a9d2da9accdb6749ac7032
Sergey Zhigar <jedai38@gmail.com>
2022-07-17 15:05:22 +0300

    PreBuild Browser options need update v2.2

--- src/test/java/pages/BoardPage.java
+++ src/test/java/pages/BoardPage.java
@@ -97,7 +97,7 @@ public class BoardPage extends BasePage {
     }
 
     public WebElement getOpenIssueButtonLocator(String issueName) {
-        return waitsService.waitForPresenceOfElement(By.xpath(openIssueButtonLocator.replace("Replace", issueName)));
+        return waitsService.waitForVisibilityLocatedBy(By.xpath(openIssueButtonLocator.replace("Replace", issueName)));
     }
 
     public WebElement getIssueOptionsButtonLocator() {
